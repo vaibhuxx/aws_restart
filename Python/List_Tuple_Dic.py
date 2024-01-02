431391e547f3e20df33eed4ca73774221940c32d
Vaibhavi Channe <78257652+vaibhuxx@users.noreply.github.com>
2024-01-02 12:16:29 +0530

    Add files via upload

---
+++ Python/List_Tuple_Dic.py
@@ -0,0 +1,34 @@
+#Lab111
+
+#Exercise 1
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+#Exercise 2
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+#Exercise 3
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
+
---
+++ Python/StringDataType.py
@@ -0,0 +1,22 @@
+
+#Lab 110
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+#Exercise 3
+name = input("What is your name? ")
+print(name)
+
+#Exercise 4
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
+
